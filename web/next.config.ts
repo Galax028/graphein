9de126a4c5a88f5566633bf094baf97a1b8548a5
PixelEdition <pixel.pxed@gmail.com>
2025-05-07 13:37:11 +0700

    feat(web): auto-redirect for signed in users

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,21 +1,23 @@
 import type { NextConfig } from "next";
+const { i18n } = require("./next-i18next.config");
 
 const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: '/client',
-        destination: '/client/dashboard',
+        source: "/client",
+        destination: "/client/dashboard",
         permanent: true,
       },
       {
-        source: '/merchant',
-        destination: '/merchant/dashboard',
+        source: "/merchant",
+        destination: "/merchant/dashboard",
         permanent: true,
       },
-    ]
+    ];
   },
   reactStrictMode: true,
+  i18n,
 };
 
 export default nextConfig;
