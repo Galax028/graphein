13d39d39474cc6a063a1d003cb99b23c1714aafc
PixelEdition <pixel.pxed@gmail.com>
2025-05-14 14:49:52 +0700

    feat: even better `NumberInput`

--- web/components/common/NumberInput.tsx
+++ web/components/common/NumberInput.tsx
@@ -13,32 +13,39 @@ type NumberInputProps = {
 function NumberInput({ count, setCount, min, max }: NumberInputProps) {
   const [tempCount, setTempCount] = useState(String(count));
 
+  // Get the value within range.
+  const clampedValue = (i: number) =>
+    Math.min(Math.max(i, min ?? -Infinity), max ?? Infinity);
+
   // Buttons
-  const updateCount = (newCount: number) => {
-    const clampedValue = Math.min(
-      Math.max(newCount, min ?? -Infinity),
-      max ?? Infinity
-    );
-    setTempCount(String(clampedValue));
-    setCount(clampedValue);
+  const updateCount = (i: number) => {
+    setTempCount(String(clampedValue(i)));
+    setCount(clampedValue(i));
   };
 
   // Text Input
-  const validateUpdateCount = (value: string) => {
-    const tempNumber = Number(value.replace(/[^\d-]/g, ""));
+  const validateUpdateCount = (i: string) => {
+    let rawString = i.replace(/^0+/, "");
 
+    // Remove non-numbers except "-"
+    const tempNumber = Number(
+      rawString
+        ? rawString[0] === "-"
+          ? "-" + rawString.slice(1).replace(/[^0-9]/g, "")
+          : rawString.replace(/[^0-9]/g, "")
+        : rawString
+    );
+
+    // If the number is not valid, set it to 0.
+    // Note: If 0 is not in range, we set it to min or max afterwards.
     if (isNaN(tempNumber)) {
       setTempCount("0");
       setCount(0);
       return;
     }
 
-    const clampedValue = Math.min(
-      Math.max(tempNumber, min ?? -Infinity),
-      max ?? Infinity
-    );
-    setTempCount(String(clampedValue));
-    setCount(clampedValue);
+    setTempCount(String(clampedValue(tempNumber)));
+    setCount(clampedValue(tempNumber));
   };
 
   return (
@@ -48,7 +55,9 @@ function NumberInput({ count, setCount, min, max }: NumberInputProps) {
         appearance="tonal"
         icon="remove"
         disabled={min != undefined && count <= min}
-        onClick={() => {updateCount(count - 1)}}
+        onClick={() => {
+          updateCount(count - 1);
+        }}
       />
       <input
         className={cn(
@@ -70,7 +79,9 @@ function NumberInput({ count, setCount, min, max }: NumberInputProps) {
         appearance="tonal"
         icon="add"
         disabled={max != undefined && count >= max}
-        onClick={() => {updateCount(count + 1)}}
+        onClick={() => {
+          updateCount(count + 1);
+        }}
       />
     </SegmentedGroup>
   );
