07a49fbd25007d814aa39d3af858e8b3dc1c5388
PixelEdition <pixel.pxed@gmail.com>
2025-05-26 10:23:14 +0700

    feat(web): settings page skeleton

--- web/pages/settings.tsx
+++ web/pages/settings.tsx
@@ -1,16 +1,24 @@
 import Button from "@/components/common/Button";
-import getLoggedInUser from "@/utils/helpers/getLoggedInUser";
-import { GetServerSideProps } from "next";
+import InputLabel from "@/components/common/InputLabel";
 import NavigationBar from "@/components/common/NavigationBar";
-import { useState } from "react";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import PersonAvatar from "@/components/common/PersonAvatar";
+import SegmentedGroup from "@/components/common/SegmentedGroup";
 
-const SettingsPage = (user: any) => {
+const SettingsPage = () => {
   const router = useRouter();
-  const [busy, setBusy] = useState<boolean>(false)
+
+  const [user, setUser] = useState<any>({});
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+  const [busy, setBusy] = useState<boolean>(false);
+
+  const [phone, setPhone] = useState<string>();
+  const [classroom, setClassroom] = useState<string>("");
+  const [classNo, setClassNo] = useState<string>("");
 
   const handleSignOut = async () => {
-    setBusy(true)
+    setBusy(true);
 
     const res = await fetch(
       process.env.NEXT_PUBLIC_API_PATH + "/auth/signout",
@@ -25,29 +33,123 @@ const SettingsPage = (user: any) => {
     }
   };
 
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await fetch(process.env.NEXT_PUBLIC_API_PATH + "/user", {
+        method: "GET",
+        credentials: "include",
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        if (isSignedIn == false) {
+          setIsSignedIn(true);
+          setUser(data);
+        }
+      }
+    };
+
+    getUser();
+  }, []);
+
   return (
     <>
       <NavigationBar title="Settings" backEnabled={true} />
-      <div className="p-3">
-        <p>{JSON.stringify(user)}</p>
+      <div className="flex flex-col gap-3 p-3">
+        {isSignedIn && (
+          <>
+            <InputLabel
+              header="About you"
+              footer="You cannot change your profile picture, name, or email here because they’re synced with your Google account—please update them in your Google account settings."
+            >
+              <div className="flex flex-col gap-3 p-3 bg-surface-container border border-outline rounded-lg">
+                <InputLabel header="Profile">
+                  <div className="m-auto">
+                    <PersonAvatar
+                      profile_url={user.data.profileUrl}
+                      person_name={user.data.name}
+                      size={96}
+                    />
+                  </div>
+                </InputLabel>
+                <InputLabel header="Name">
+                  <input
+                    value={user.data.name}
+                    className="w-full p-2 bg-background border border-outline rounded-lg text-body-md h-10 text-on-background-disabled"
+                    disabled
+                  />
+                </InputLabel>
+                <InputLabel header="Email">
+                  <input
+                    value={user.data.email}
+                    className="w-full p-2 bg-background border border-outline rounded-lg text-body-md h-10 text-on-background-disabled"
+                    disabled
+                  />
+                </InputLabel>
+                <InputLabel header="Phone">
+                  <input
+                    value={user.data.tel}
+                    className="w-full p-2 bg-background border border-outline rounded-lg text-body-md h-10"
+                  />
+                </InputLabel>
+                <InputLabel header="Class / No.">
+                  <SegmentedGroup>
+                    <div className="text-body-md flex items-center justify-center p-2 h-10 aspect-square bg-surface-container border border-outline">
+                      <p>M.</p>
+                    </div>
+                    <input
+                      value={user.data.class}
+                      onChange={(e) => {
+                        setClassroom(e.target.value);
+                      }}
+                      type="text"
+                      className="w-full p-2 bg-background"
+                    />
+                    <div className="text-body-md flex items-center justify-center p-2 h-10 aspect-square bg-surface-container border border-outline">
+                      <p>No.</p>
+                    </div>
+                    <input
+                      value={user.data.classNo}
+                      onChange={(e) => {
+                        setClassNo(e.target.value);
+                      }}
+                      type="text"
+                      className="w-full p-2 bg-background"
+                    />
+                  </SegmentedGroup>
+                </InputLabel>
+              </div>
+            </InputLabel>
+          </>
+        )}
         <Button
           appearance="tonal"
           onClick={handleSignOut}
-          className="w-full text-actionError"
+          className="w-full text-error"
           icon={"logout"}
           busy={busy}
         >
           Sign Out
         </Button>
+        <InputLabel header="Developer Log">
+          <div className="p-3 text-body-sm bg-surface-container border border-outline rounded-lg">
+            <b>
+              <a
+                className="!font-mono break-all"
+                href={process.env.NEXT_PUBLIC_API_PATH + "/user"}
+                target="_blank"
+              >
+                {process.env.NEXT_PUBLIC_API_PATH + "/user"}
+              </a>
+            </b>
+            <br />
+            <span className="!font-mono break-all">{JSON.stringify(user)}</span>
+          </div>
+        </InputLabel>
       </div>
     </>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async (req) => {
-  const user = await getLoggedInUser(req);
-
-  return { props: { user } };
 };
 
 export default SettingsPage;
