b7f7bdd4950d339808985e325977d334bca4c0dd
PixelEdition <pixel.pxed@gmail.com>
2025-05-28 14:44:01 +0700

    feat(web): `getDateString` helper

---
+++ web/utils/helpers/getDateString.ts
@@ -0,0 +1,18 @@
+export default function getDateString(date: Date) {
+  return `${date.getDay()} ${
+    [
+      "January",
+      "Febuary",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ][date.getMonth()]
+  } ${date.getFullYear()}`;
+}
