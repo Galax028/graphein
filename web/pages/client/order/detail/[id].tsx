ca67e968fb823284161d5c286b761ce29da702af
PixelEdition <pixel.pxed@gmail.com>
2025-05-30 10:43:53 +0700

    feat(web): `/client/order/detail/{id}` phase 1

--- web/pages/client/order/detail/[id].tsx
+++ web/pages/client/order/detail/[id].tsx
@@ -1,7 +1,15 @@
-import { useRouter } from "next/router";
+import OrderCard from "@/components/client/dashboard/OrderCard";
+import DropDownCard from "@/components/common/DropDownCard";
+import LabelGroup from "@/components/common/LabelGroup";
 import NavigationBar from "@/components/common/NavigationBar";
-import { GetServerSideProps } from "next";
+import cn from "@/utils/helpers/cn";
+import getDateTimeString from "@/utils/helpers/getDateTimeString";
 import getLoggedInUser from "@/utils/helpers/getLoggedInUser";
+import { getOrderStatusFromTimestamp } from "@/utils/helpers/getOrderStatusFromTimestamp";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { motion } from "motion/react";
 
 type OrderDetailsPageProps = {
   user: any;
@@ -10,14 +18,193 @@ type OrderDetailsPageProps = {
 const OrderDetailsPage = ({ user }: OrderDetailsPageProps) => {
   const router = useRouter();
 
+  const [detailedState, setDetailedState] = useState<any>({});
+  useEffect(() => {
+    const fetchDetailedOrder = async () => {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_PATH}/orders/${router.query.id}`,
+        {
+          credentials: "include",
+        }
+      );
+
+      const data = await res.json();
+      setDetailedState(data);
+    };
+
+    fetchDetailedOrder();
+  }, []);
+
+  // Rename pls!!!!
+  const createdTimestamp = new Date(detailedState.data?.createdAt);
+  const aboutOrderProps = [
+    {
+      title: "Created",
+      content: detailedState.data ? getDateTimeString(createdTimestamp) : "-",
+    },
+    {
+      title: "Price",
+      content: detailedState.data?.price ?? "-",
+    },
+    {
+      title: "Order ID",
+      content: detailedState.data
+        ? `${String(createdTimestamp.getDate()).padStart(2, "0")}${String(
+            createdTimestamp.getMonth()
+          ).padStart(
+            2,
+            "0"
+          )}${createdTimestamp.getFullYear()}-${detailedState.data.orderNumber.replace(
+            /\-/g,
+            ""
+          )}`
+        : "-",
+    },
+  ];
+  const timeLogProps = [
+    {
+      title: "Reviewing",
+      content: detailedState.data
+        ? getDateTimeString(
+            new Date(getOrderStatusFromTimestamp(detailedState, "reviewing"))
+          )
+        : "-",
+    },
+    {
+      title: "Printing",
+      content: detailedState.data
+        ? getDateTimeString(
+            new Date(getOrderStatusFromTimestamp(detailedState, "processing"))
+          )
+        : "-",
+    },
+    {
+      title: "Pickup",
+      content: detailedState.data
+        ? getDateTimeString(
+            new Date(getOrderStatusFromTimestamp(detailedState, "ready"))
+          )
+        : "-",
+    },
+    {
+      title: detailedState.data
+        ? getOrderStatusFromTimestamp(detailedState, "rejected")
+          ? "Rejected"
+          : getOrderStatusFromTimestamp(detailedState, "cancelled")
+          ? "Cancelled"
+          : "Completed"
+        : "Completed",
+      content: detailedState.data
+        ? detailedState.data.statusHistory?.rejected
+          ? getDateTimeString(
+              new Date(getOrderStatusFromTimestamp(detailedState, "rejected"))
+            )
+          : detailedState.data.statusHistory?.cancelled
+          ? getDateTimeString(
+              new Date(getOrderStatusFromTimestamp(detailedState, "cancelled"))
+            )
+          : getDateTimeString(
+              new Date(getOrderStatusFromTimestamp(detailedState, "completed"))
+            )
+        : "-",
+    },
+  ];
+
   return (
     <>
       <NavigationBar
-        title={`Order #${router.query.id}`}
+        title={`Order #${detailedState.data?.orderNumber ?? ""}`}
         backEnabled={true}
         user={user}
       />
-      <main>{router.query.id}</main>
+      <main className="p-3 flex flex-col gap-3">
+        {detailedState.data && (
+          <motion.div
+            initial={{ opacity: 0, y: 64 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              y: { type: "spring", bounce: 0 },
+            }}
+            className="flex flex-col gap-3"
+          >
+            <LabelGroup header="Your Order">
+              <OrderCard
+                status={detailedState.data?.status ?? "unknown"}
+                orderNumber={detailedState.data?.orderNumber}
+                createdAt={detailedState.data?.createdAt}
+                filesCount={detailedState.data?.files.length}
+                options={{
+                  showProgressBar: true,
+                }}
+              />
+              <DropDownCard header="About Order">
+                <div className="grid grid-cols-[4.5rem_1fr] gap-x-4 gap-y-2">
+                  {aboutOrderProps.map((i) => (
+                    <>
+                      <p className="w-18 text-body-sm opacity-50">{i.title}</p>
+                      <p className="w-full text-body-md">{i.content}</p>
+                    </>
+                  ))}
+                </div>
+              </DropDownCard>
+              <DropDownCard header="Time Log">
+                <div className="grid grid-cols-[4.5rem_1fr] gap-x-4 gap-y-2">
+                  {timeLogProps.map((i) => (
+                    <>
+                      <p className="text-body-sm opacity-50">{i.title}</p>
+                      <p className="text-body-md">{i.content}</p>
+                    </>
+                  ))}
+                </div>
+              </DropDownCard>
+            </LabelGroup>
+            <LabelGroup header="Note to Shop">
+              <div
+                className={cn(
+                  `p-3 bg-surface-container border border-outline rounded-lg`
+                )}
+              >
+                <p className="text-body-md">
+                  {detailedState.data?.notes ?? (
+                    <span className="opacity-50 italic">
+                      No notes provided.
+                    </span>
+                  )}
+                </p>
+              </div>
+            </LabelGroup>
+          </motion.div>
+        )}
+
+        {process.env.NODE_ENV === "development" && (
+          <LabelGroup header="Developer Log">
+            <div
+              className={cn(
+                `p-3 border border-outline bg-surface-container rounded-lg 
+              text-body-sm`
+              )}
+            >
+              <b>
+                <a
+                  className="!font-mono break-all"
+                  href={
+                    process.env.NEXT_PUBLIC_API_PATH +
+                    "/orders/844d2794-e378-4c77-b1bc-d5ff9685c744"
+                  }
+                  target="_blank"
+                >
+                  {process.env.NEXT_PUBLIC_API_PATH +
+                    "/orders/844d2794-e378-4c77-b1bc-d5ff9685c744"}
+                </a>
+              </b>
+              <br />
+              <span className="!font-mono break-all">
+                {JSON.stringify(detailedState)}
+              </span>
+            </div>
+          </LabelGroup>
+        )}
+      </main>
     </>
   );
 };
