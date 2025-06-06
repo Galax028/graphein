965dee990b9339390c29b8b1c589dcc9a78db0bb
PixelEdition <pixel.pxed@gmail.com>
2025-06-06 14:57:35 +0700

    feat(web): `getShortenedFileSizeString` helper function

---
+++ web/utils/helpers/getShortenedFileSizeString.ts
@@ -0,0 +1,15 @@
+export function getShortenedFileSizeString(i: number) {
+  let number = i
+  if (i < 1000) {
+    return `${number} B`
+  } else if (i < Math.pow(1000, 2)) {
+    number = i / 1000
+    return `${Math.floor(number * 10) / 10} KB`
+  } else if (i < Math.pow(1000, 3)) {
+    number = i / (1000 * 1000)
+    return `${Math.floor(number * 10) / 10} MB`
+  } else if (i < Math.pow(1000, 4)) {
+    number = i / (1000 * 1000)
+    return `${Math.floor(number * 10) / 10} GB`
+  }
+}
