2591880edf21896c6a4d7663e9217c64da7ca293
pixelpxed <pixel.pxed@gmail.com>
2025-07-01 13:48:10 +0700

    feat(web): `serverSideTranslations.ts` accept multiple files

--- web/utils/helpers/serverSideTranslations.ts
+++ web/utils/helpers/serverSideTranslations.ts
@@ -5,17 +5,27 @@ const getServerSideTranslations = async (
   req: IncomingMessage & {
     cookies: NextApiRequestCookies;
   },
-  route: string,
-): Promise<[string, Record<string, string>]> => {
+  routes: string | string[],
+): Promise<[string, Record<string, any>]> => {
   const locale =
     req.cookies["NEXT_LOCALE"] ??
     process.env.NEXT_PUBLIC_DEFAULT_LOCALE ??
     "en";
 
-  return [
-    locale,
-    (await import(`@/translations/${locale}/${route}.json`)).default,
-  ];
+  let translations: Record<string, any> = {};
+
+  if (Array.isArray(routes)) {
+    for (const route of routes) {
+      translations[route] = (
+        await import(`@/translations/${locale}/${route}.json`)
+      ).default;
+    }
+  } else {
+    translations = (await import(`@/translations/${locale}/${routes}.json`))
+      .default;
+  }
+
+  return [locale, translations];
 };
 
 export default getServerSideTranslations;
