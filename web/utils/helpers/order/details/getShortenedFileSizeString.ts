bcefeac556ba7fee377cb8d6e584070b437a7b9d
pixelpxed <pixel.pxed@gmail.com>
2025-07-01 10:39:12 +0700

    fix(web), refactor(web): `getShortenedFileSizeString.ts` GB is miscalculated, refactored code

--- web/utils/helpers/order/details/getShortenedFileSizeString.ts
+++ web/utils/helpers/order/details/getShortenedFileSizeString.ts
@@ -1,15 +1,12 @@
-export function getShortenedFileSizeString(i: number) {
-  let number = i;
-  if (i < 1000) {
-    return `${number} B`;
-  } else if (i < Math.pow(1000, 2)) {
-    number = i / 1000;
-    return `${Math.floor(number * 10) / 10} KB`;
-  } else if (i < Math.pow(1000, 3)) {
-    number = i / (1000 * 1000);
-    return `${Math.floor(number * 10) / 10} MB`;
-  } else if (i < Math.pow(1000, 4)) {
-    number = i / (1000 * 1000);
-    return `${Math.floor(number * 10) / 10} GB`;
+export function getShortenedFileSizeString(i: number): string {
+  const units = ["B", "KB", "MB", "GB", "TB"];
+  let index = 0;
+  let size = i;
+
+  while (size >= 1000 && index < units.length - 1) {
+    size /= 1000;
+    index++;
   }
+
+  return `${Math.floor(size * 10) / 10} ${units[index]}`;
 }
