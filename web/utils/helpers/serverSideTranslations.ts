a0cb13bdf54e08e48d7060b08bf17bcf05662019
Phawat Suksiriwan <phawat@suksiriwan.com>
2025-07-01 20:51:10 +0700

    fix(web): fix error due to recent merge conflict

--- web/utils/helpers/serverSideTranslations.ts
+++ web/utils/helpers/serverSideTranslations.ts
@@ -6,21 +6,24 @@ const getServerSideTranslations = async (
   req: IncomingMessage & {
     cookies: NextApiRequestCookies;
   },
-  route: string,
+  routes: string,
 ): Promise<[string, TranslationRecord]> => {
   const locale =
     req.cookies["NEXT_LOCALE"] ??
     process.env.NEXT_PUBLIC_DEFAULT_LOCALE ??
     "en";
 
-  let translations: Record<string, any> = {};
+  let translations = {} as TranslationRecord;
 
   if (Array.isArray(routes)) {
-    for (const route of routes) {
-      translations[route] = (
-        await import(`@/translations/${locale}/${route}.json`)
-      ).default;
-    }
+    await Promise.all(
+      routes.map(
+        async (route) =>
+          (translations[route] = (
+            await import(`@/translations/${locale}/${route}.json`)
+          ).default),
+      ),
+    );
   } else {
     translations = (await import(`@/translations/${locale}/${routes}.json`))
       .default;
