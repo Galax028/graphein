c4cef42e44481016657b0e64f9b6d445818ba92a
Phawat Suksiriwan <phawat@suksiriwan.com>
2025-07-03 23:32:10 +0700

    fix(web): fix weird styling for history

--- web/pages/order/history.tsx
+++ web/pages/order/history.tsx
@@ -9,11 +9,10 @@ import {
   useOrderHistoryInfiniteQuery,
 } from "@/query/fetchOrderHistory";
 import { prefetchUser } from "@/query/fetchUser";
-import cn from "@/utils/helpers/cn";
 import getServerSideTranslations from "@/utils/helpers/serverSideTranslations";
 import type { PageProps } from "@/utils/types/common";
 import useUserContext from "@/utils/useUserContext";
-import { QueryClient } from "@tanstack/react-query";
+import { dehydrate, QueryClient } from "@tanstack/react-query";
 import { motion } from "motion/react";
 import type { GetServerSideProps } from "next";
 import Link from "next/link";
@@ -35,65 +34,58 @@ const OrderHistoryPage: FC<PageProps> = () => {
   if (status === "pending" || status === "error") return <></>;
 
   return (
-    <div className="flex flex-col h-dvh overflow-hidden">
+    <>
       <NavigationBar
         user={user}
         title="Order History"
         backEnabled={true}
         backContextURL={"/glance"}
       />
-      <PageLoadTransition className="flex flex-col h-full w-full overflow-auto gap-3 font-mono">
-        <div
-          className={cn(
-            `flex flex-col p-3 gap-2 [&>div]:w-full h-full overflow-auto pb-16`,
+      <PageLoadTransition>
+        <LabelGroup header={"Previous 30 days"}>
+          {data.pages.length === 0 ? (
+            <OrderEmptyCard text={`Orders completed will appear here.`} />
+          ) : (
+            data.pages
+              .flatMap((page) => page.orders)
+              .map((order, idx) => (
+                <motion.div
+                  initial={{ opacity: 0, y: 16 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{
+                    y: { type: "spring", bounce: 0 },
+                    delay: idx * 0.2,
+                  }}
+                  key={idx}
+                >
+                  <Link href={`/order/detail/${order.id}`}>
+                    <OrderCard
+                      status={order.status}
+                      orderNumber={order.orderNumber}
+                      createdAt={order.createdAt}
+                      filesCount={order.filesCount}
+                      options={{
+                        showNavigationIcon: false,
+                      }}
+                    />
+                  </Link>
+                </motion.div>
+              ))
           )}
+        </LabelGroup>
+        <Button
+          appearance="tonal"
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetching}
         >
-          <LabelGroup header={"Previous 30 days"}>
-            {data.pages.length === 0 ? (
-              <OrderEmptyCard text={`Orders completed will appear here.`} />
-            ) : (
-              data.pages
-                .flatMap((page) => page.orders)
-                .map((order, idx) => (
-                  <motion.div
-                    initial={{ opacity: 0, y: 16 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{
-                      y: { type: "spring", bounce: 0 },
-                      delay: idx * 0.2,
-                    }}
-                    key={idx}
-                  >
-                    <Link href={`/order/detail/${order.id}`}>
-                      <OrderCard
-                        status={order.status}
-                        orderNumber={order.orderNumber}
-                        createdAt={order.createdAt}
-                        filesCount={order.filesCount}
-                        options={{
-                          showNavigationIcon: false,
-                        }}
-                      />
-                    </Link>
-                  </motion.div>
-                ))
-            )}
-          </LabelGroup>
-          <Button
-            appearance="tonal"
-            className="w-full"
-            onClick={() => fetchNextPage()}
-            disabled={!hasNextPage || isFetching}
-          >
-            {isFetchingNextPage
-              ? "Loading..."
-              : hasNextPage
-                ? "Load More"
-                : "No more items"}
-          </Button>
-        </div>
+          {isFetchingNextPage
+            ? "Loading..."
+            : hasNextPage
+              ? "Load More"
+              : "No more items"}
+        </Button>
       </PageLoadTransition>
-    </div>
+    </>
   );
 };
 
@@ -101,19 +93,27 @@ export const getServerSideProps: GetServerSideProps<PageProps> = async (
   context,
 ) => {
   // TODO: translations
-  const [locale, translations] = await getServerSideTranslations(
-    context.req,
-    "glance",
-  );
+  const [locale, translations] = await getServerSideTranslations(context.req, [
+    "common",
+    "index", // TODO
+  ]);
 
   const queryClient = new QueryClient();
   const sessionToken = `session_token=${context.req.cookies["session_token"]}`;
   const signedIn = await prefetchUser(queryClient, sessionToken);
-  if (signedIn) await prefetchOrderHistory(queryClient, sessionToken);
+  if (signedIn) {
+    await prefetchOrderHistory(queryClient, sessionToken);
 
-  return signedIn
-    ? { props: { locale, translations } }
-    : { redirect: { destination: "/", permanent: false } };
+    return {
+      props: {
+        locale,
+        translations,
+        dehydratedState: dehydrate(queryClient),
+      },
+    };
+  }
+
+  return { redirect: { destination: "/", permanent: false } };
 };
 
 export default OrderHistoryPage;
