95afb4a6302bde85e62286614870f9201c8ff80f
Phawat Suksiriwan <phawat@suksiriwan.com>
2025-07-26 18:44:54 +0700

    feat(scripts): scripts now in rust

---
+++ scripts/src/main.rs
@@ -0,0 +1,14 @@
+#![forbid(unsafe_code)]
+#![warn(clippy::pedantic)]
+
+use anyhow::Result as AnyhowResult;
+use clap::Parser as _;
+use scripts::{Cli, run_command};
+
+#[tokio::main]
+async fn main() -> AnyhowResult<()> {
+    dotenvy::dotenv().ok();
+    let cli = Cli::parse();
+
+    run_command(cli).await
+}
