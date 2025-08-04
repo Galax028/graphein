13d782556e38001e922718d2077d4da96d0cb466
pixelpxed <pixel.pxed@gmail.com>
2025-08-04 11:12:22 +0700

    refactor(web): `FileDetailRange`

--- web/components/common/FileDetailRange.tsx
+++ web/components/common/FileDetailRange.tsx
@@ -6,7 +6,8 @@ type FileDetailRangeProps = {
   details: {
     title: string;
     content: string;
-  }[];}
+  }[];
+};
 
 /**
  * The configuration options for each PDF, or image file.
@@ -15,28 +16,27 @@ type FileDetailRangeProps = {
  * @param details The details within of a card. (title: str; content: str;)
  */
 
-const FileDetailRange = ({label, value, details}: FileDetailRangeProps) => {
+const FileDetailRange = ({ label, value, details }: FileDetailRangeProps) => {
   // TODO: Add localization to these types
   const labelNames = {
-    "page": "Page",
-    "image": "Image",
-    "service": "Service"
-  }
+    page: "Page",
+    image: "Image",
+    service: "Service",
+  };
 
   return (
     <div className="bg-surface-container border border-outline rounded-lg">
       <div className="flex">
-        <div className="border-r border-outline text-body-md px-3 py-2">{labelNames[label]}</div>
+        <div className="border-r border-outline text-body-md px-3 py-2">
+          {labelNames[label]}
+        </div>
         <div className="text-body-md px-3 py-2">{value}</div>
       </div>
       <div className="p-3 border-t border-outline">
-        <DescriptionList 
-          list={details}
-          expand={true}
-        />
+        <DescriptionList list={details} expand={true} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default FileDetailRange;
